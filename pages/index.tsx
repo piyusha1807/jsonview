cd8bfd2ce35aabd472b011e5282024c2eb72769d
unknown <piyushagrawal1807@gmail.com>
2023-10-27 10:39:42 +0530

    added seo title

--- pages/index.tsx
+++ pages/index.tsx
@@ -62,12 +62,15 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Online JSON Viewer</title>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title>
+          JSON Viewer Online: Open, view, Format, and Share JSON File
+        </title>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="description"
-          content="Online JSON Viewer helps the user to format data and view it as a tree structure. This allows users to identify errors and fix them quickly."
+          content="JSON Viewer Online is a free and easy-to-use web app for developers to import, create, edit, view, format, minify, debug, save, and share json data."
         />
         <meta
           name="keywords"
@@ -76,19 +79,26 @@ export default function Home() {
         />
         <meta name="robots" content="index, follow" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Online JSON Viewer" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="JSON Viewer Online: Open, view, Format, and Share JSON File"
+        />
         <meta property="og:url" content="https://jsonviewer.info" />
         <meta
           property="og:description"
-          content="Online JSON Viewer helps the user to format data and view it as a tree structure. This allows users to identify errors and fix them quickly."
+          content="JSON Viewer Online is a free and easy-to-use web app for developers to import, create, edit, view, format, minify, debug, save, and share json data."
         />
         <meta property="og:image" content="https://i.imgur.com/Hh1vjr7.png" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@username" />
-        <meta name="twitter:title" content="Online JSON Viewer" />
+        <meta
+          name="twitter:title"
+          content="JSON Viewer Online: Open, view, Format, and Share JSON File"
+        />
         <meta
           name="twitter:description"
-          content="Online JSON Viewer helps the user to format data and view it as a tree structure. This allows users to identify errors and fix them quickly."
+          content="JSON Viewer Online is a free and easy-to-use web app for developers to import, create, edit, view, format, minify, debug, save, and share json data."
         />
         <meta name="twitter:image" content="https://i.imgur.com/Hh1vjr7.png" />
         <link rel="canonical" href="https://jsonviewer.info" />
