5a374634a2151a26f2b26aed3598d5b78a00c9f5
Piyush Agrawal <62530619+piyusha1807@users.noreply.github.com>
2023-11-18 19:51:24 +0000

    changed react view library

--- components/jsontool.tsx
+++ components/jsontool.tsx
@@ -8,16 +8,51 @@ import {
   setInputError,
   setOutputData,
 } from "@/store/actions/dashboardAction";
-import { Box, useMantineColorScheme } from "@mantine/core";
+import { useMantineColorScheme } from "@mantine/core";
+import JsonView from "@uiw/react-json-view";
+import { lightTheme } from "@uiw/react-json-view/light";
 
 const Editor = dynamic(() => import("@monaco-editor/react"), {
   ssr: false,
   loading: () => <div className="loading">Loading...</div>,
 });
 
-const DynamicReactJson = dynamic(() => import("react-json-view"), {
-  ssr: false,
-});
+const Quote = JsonView.Quote;
+
+const customLightTheme = {
+  ...lightTheme,
+  "--w-rjv-background-color": "#f1f3f5",
+};
+
+const customDarkTheme = {
+  "--w-rjv-color": "#9cdcfe",
+  "--w-rjv-key-string": "#9cdcfe",
+  "--w-rjv-background-color": "#25262b",
+  "--w-rjv-line-color": "#36334280",
+  "--w-rjv-arrow-color": "#838383",
+  "--w-rjv-edit-color": "#9cdcfe",
+  "--w-rjv-info-color": "#9c9c9c7a",
+  "--w-rjv-update-color": "#9cdcfe",
+  "--w-rjv-copied-color": "#9cdcfe",
+  "--w-rjv-copied-success-color": "#28a745",
+
+  "--w-rjv-curlybraces-color": "#d4d4d4",
+  "--w-rjv-colon-color": "#d4d4d4",
+  "--w-rjv-brackets-color": "#d4d4d4",
+  "--w-rjv-quotes-color": "#9cdcfe",
+  "--w-rjv-quotes-string-color": "#ce9178",
+
+  "--w-rjv-type-string-color": "#ce9178",
+  "--w-rjv-type-int-color": "#b5cea8",
+  "--w-rjv-type-float-color": "#b5cea8",
+  "--w-rjv-type-bigint-color": "#b5cea8",
+  "--w-rjv-type-boolean-color": "#569cd6",
+  "--w-rjv-type-date-color": "#b5cea8",
+  "--w-rjv-type-url-color": "#3b89cf",
+  "--w-rjv-type-null-color": "#569cd6",
+  "--w-rjv-type-nan-color": "#859900",
+  "--w-rjv-type-undefined-color": "#569cd6",
+};
 
 const MONACO_OPTIONS = {
   fontFamily: "MonoLisa, monospace",
@@ -107,23 +142,24 @@ function Jsontool() {
           theme={colorScheme === "light" ? "light" : "vs-dark"}
         />
       </div>
-      <div className="outputArea">
+      <div className="outputArea" id="text-container">
         {outputData && typeof outputData === "object" && (
-          <DynamicReactJson
-            src={outputData}
-            theme={colorScheme === "light" ? "rjv-default" : "chalk"}
+          <JsonView
+            value={outputData}
             displayDataTypes={false}
-            indentWidth={2}
             collapsed={2}
             enableClipboard={false}
-            groupArraysAfterLength={0}
-            quotesOnKeys={false}
             displayObjectSize={settingsConfig?.displayChildrenCount ?? true}
             style={{
+              ...(colorScheme === "light" ? customLightTheme : customDarkTheme),
               height: "100%",
               overflow: "auto",
             }}
-          />
+          >
+            <Quote>
+              <span />
+            </Quote>
+          </JsonView>
         )}
       </div>
     </Split>
